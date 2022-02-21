73eafb407532640566b5f1cc26ef29aed7c714ab
CondorW <rotsteiner@gmail.com>
2022-02-21 20:08:27 +0100

    Install Tailwind - basic "design" setup and app structure

---
+++ src/Components/Films.js
@@ -0,0 +1,11 @@
+const Films = () =>{
+    return(
+        <div className="bg-lime-200 w-1/3 h-1/3 flex items-center justify-center flex-col">
+            <div>Film Numero Uno</div>
+            <div>Film Numero Dos</div>
+            <button className="block bg-lime-400">Give me those sweet Film Suggestions</button>
+        </div>
+    )
+
+}
+export default Films;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return <header>
+      <nav className="bg-lime-300 flex justify-between">
+          <h1>StarWarsFilmFinder - SWFF</h1>
+          <h2>Probably the ugliest FilmFinder on the web</h2>
+      </nav>
+  </header>;
+}
