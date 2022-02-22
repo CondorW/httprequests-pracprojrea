7e8198af53fa8cfd40a10be931b45bd927d39757
CondorW <rotsteiner@gmail.com>
2022-02-22 17:45:16 +0100

    Fix bug where code fails to compile at launch when there is no data in the state variable

--- src/Components/Films.js
+++ src/Components/Films.js
@@ -2,20 +2,22 @@ import { useState } from "react";
 import FilmCard from "./FilmCard";
 
 const Films = () => {
-  const [filmDataState, setFilmDataState] = useState({});
+  const [filmDataState, setFilmDataState] = useState();
 
   const getFilmsHandler = () => {
     fetch("https://swapi.dev/api/films/")
       .then((response) => response.json())
-      .then((data) => setFilmDataState(data))
+      .then((data) => setFilmDataState(data));
   };
   console.log(filmDataState);
 
   return (
     <div className="bg-lime-200 w-1/3 h-1/3 flex items-center justify-center flex-col">
-      {filmDataState.results.map((film,index) => {
-        return <FilmCard key={index} title={film.title}></FilmCard>;
-      })}
+      {filmDataState !== undefined
+        ? filmDataState.results.map((film, index) => {
+            return <FilmCard key={index} title={film.title}></FilmCard>;
+          })
+        : null}
       <FilmCard></FilmCard>
       <button onClick={getFilmsHandler} className="block bg-lime-400">
         Give me those sweet Film Suggestions
