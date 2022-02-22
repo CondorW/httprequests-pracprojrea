1f9493931b32e2175da1caf43245588de3febadd
CondorW <rotsteiner@gmail.com>
2022-02-22 17:23:21 +0100

    Basic API functionality working - with a click on the button the titles of the fetched films get displayed

--- src/Components/Films.js
+++ src/Components/Films.js
@@ -1,11 +1,26 @@
-const Films = () =>{
-    return(
-        <div className="bg-lime-200 w-1/3 h-1/3 flex items-center justify-center flex-col">
-            <div>Film Numero Uno</div>
-            <div>Film Numero Dos</div>
-            <button className="block bg-lime-400">Give me those sweet Film Suggestions</button>
-        </div>
-    )
+import { useState } from "react";
+import FilmCard from "./FilmCard";
 
-}
+const Films = () => {
+  const [filmDataState, setFilmDataState] = useState({});
+
+  const getFilmsHandler = () => {
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => response.json())
+      .then((data) => setFilmDataState(data))
+  };
+  console.log(filmDataState);
+
+  return (
+    <div className="bg-lime-200 w-1/3 h-1/3 flex items-center justify-center flex-col">
+      {filmDataState.results.map((film,index) => {
+        return <FilmCard key={index} title={film.title}></FilmCard>;
+      })}
+      <FilmCard></FilmCard>
+      <button onClick={getFilmsHandler} className="block bg-lime-400">
+        Give me those sweet Film Suggestions
+      </button>
+    </div>
+  );
+};
 export default Films;
