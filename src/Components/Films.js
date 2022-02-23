5a7d947fe2c4b0621371c71d07d9c77fea4e2798
CondorW <rotsteiner@gmail.com>
2022-02-23 17:36:43 +0100

    Add conditional rendering for the cases when data is being loaded and no data is available

--- src/Components/Films.js
+++ src/Components/Films.js
@@ -3,22 +3,33 @@ import FilmCard from "./FilmCard";
 
 const Films = () => {
   const [filmDataState, setFilmDataState] = useState();
+  const [loadingState, setLoadingState] = useState();
 
   async function getFilmsHandler() {
-    const response = await fetch('https://swapi.dev/api/films/');
+    setLoadingState(true);
+    const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
     setFilmDataState(data);
-  };
+    setLoadingState(false);
+  }
   console.log(filmDataState);
 
   return (
     <div className="bg-lime-200 w-3/4 rounded flex items-center justify-center flex-col mt-4">
-      {filmDataState !== undefined
-        ? filmDataState.results.map((film, index) => {
-            return <FilmCard key={index} title={film.title} crawl={film.opening_crawl}></FilmCard>;
-          })
-        : null}
-      <FilmCard></FilmCard>
+      {loadingState && <p>Please Stay Patient - data is loading</p>}
+      {loadingState === false && filmDataState !== undefined ? (
+        filmDataState.results.map((film, index) => {
+          return (
+            <FilmCard
+              key={index}
+              title={film.title}
+              crawl={film.opening_crawl}
+            ></FilmCard>
+          );
+        })
+      ) : (
+        <p>Sorry we have no data - try fetching some movies</p>
+      )}
       <button onClick={getFilmsHandler} className="block bg-lime-400">
         Give me those sweet Film Suggestions
       </button>
