ba1a4e6032cff13fa906fce94b2cf522709d095a
CondorW <rotsteiner@gmail.com>
2022-02-24 21:55:38 +0100

    Add logic to conditionally render an error message if the fetch process fails - whereas the jsx code gets stored in the toBeRendered variable, of which the value gets changed depending on how the state of the application behaves

--- src/Components/Films.js
+++ src/Components/Films.js
@@ -4,32 +4,43 @@ import FilmCard from "./FilmCard";
 const Films = () => {
   const [filmDataState, setFilmDataState] = useState();
   const [loadingState, setLoadingState] = useState();
+  const [errorState, setErrorState] = useState(false);
 
   async function getFilmsHandler() {
     setLoadingState(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    setFilmDataState(data);
-    setLoadingState(false);
-  }
-  console.log(filmDataState);
 
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      const data = await response.json();
+      setFilmDataState(data);
+      setLoadingState(false);
+    } catch (error) {
+      setErrorState(error.message);
+    }
+  }
+
+  var toBeRendered = <p>Sorry we have no data - try fetching some movies</p>;
+  if (errorState === false && filmDataState !== undefined) {
+    toBeRendered = filmDataState.results.map((film, index) => {
+      return (
+        <FilmCard
+          key={index}
+          title={film.title}
+          crawl={film.opening_crawl}
+        ></FilmCard>
+      );
+    });
+  }
+  if (errorState === false && loadingState === true) {
+    toBeRendered = <p>Please stay patient while we fetch data</p>;
+  }
+  if (errorState !== false) {
+    toBeRendered = <p>A critical error occured</p>;
+  }
+  console.log(toBeRendered);
   return (
     <div className="bg-lime-200 w-3/4 rounded flex items-center justify-center flex-col mt-4">
-      {loadingState && <p>Please Stay Patient - data is loading</p>}
-      {loadingState === false && filmDataState !== undefined ? (
-        filmDataState.results.map((film, index) => {
-          return (
-            <FilmCard
-              key={index}
-              title={film.title}
-              crawl={film.opening_crawl}
-            ></FilmCard>
-          );
-        })
-      ) : (
-        <p>Sorry we have no data - try fetching some movies</p>
-      )}
+      {toBeRendered}
       <button onClick={getFilmsHandler} className="block bg-lime-400">
         Give me those sweet Film Suggestions
       </button>
