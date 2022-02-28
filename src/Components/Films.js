64c07e5998db2ac0cf48c14582cd638d912d014c
CondorW <rotsteiner@gmail.com>
2022-02-28 10:30:00 +0100

    Refactor code of fetch function into custom http hook

--- src/Components/Films.js
+++ src/Components/Films.js
@@ -1,36 +1,9 @@
-import { useState, useEffect, useCallback } from "react";
+import { useEffect } from "react";
 import FilmCard from "./FilmCard";
+import useHttp from "../hooks/use-http";
 
 const Films = () => {
-  const [filmDataState, setFilmDataState] = useState();
-  const [loadingState, setLoadingState] = useState();
-  const [errorState, setErrorState] = useState(false);
-
-  const getFilmsHandler = useCallback(async () => {
-    setLoadingState(true);
-    try {
-      const response = await fetch(
-        process.env.REACT_APP_DBLINKMOVIES,
-        { method: "GET", headers: { "Content-Type": "application/json" }, }
-      );
-      const data = await response.json();
-      console.log(data);
-
-      let filmArray = [];
-      for(const key in data){
-        filmArray.push({
-          key: key,
-          title: data[key].title,
-          crawl: data[key].openingText
-        })
-      }
-      console.log(filmArray);
-      setFilmDataState(filmArray);
-      setLoadingState(false);
-    } catch (error) {
-      setErrorState(error.message);
-    }
-  }, []);
+  const [filmDataState, loadingState, errorState, getFilmsHandler] = useHttp();
 
   useEffect(() => {
     getFilmsHandler();
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,5 +1,35 @@
-function useHttp(){
+import { useState, useCallback } from "react";
 
-    
+function useHttp(postRequest) {
+  const [filmDataState, setFilmDataState] = useState();
+  const [loadingState, setLoadingState] = useState();
+  const [errorState, setErrorState] = useState(false);
+
+  const getFilmsHandler = useCallback(async () => {
+    setLoadingState(true);
+    try {
+      const response = await fetch(process.env.REACT_APP_DBLINKMOVIES, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      console.log(data);
+
+      let filmArray = [];
+      for (const key in data) {
+        filmArray.push({
+          key: key,
+          title: data[key].title,
+          crawl: data[key].openingText,
+        });
+      }
+      console.log(filmArray);
+      setFilmDataState(filmArray);
+      setLoadingState(false);
+    } catch (error) {
+      setErrorState(error.message);
+    }
+  }, []);
+  return [filmDataState, loadingState, errorState, getFilmsHandler];
 }
 export default useHttp;
