823fe9f6ff1c506c6fa83ae8cd3206c04240a504
CondorW <rotsteiner@gmail.com>
2022-02-28 10:37:33 +0100

    get functionality of custom http hook working

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -5,31 +5,36 @@ function useHttp(postRequest) {
   const [loadingState, setLoadingState] = useState();
   const [errorState, setErrorState] = useState(false);
 
-  const getFilmsHandler = useCallback(async () => {
-    setLoadingState(true);
-    try {
-      const response = await fetch(process.env.REACT_APP_DBLINKMOVIES, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      });
-      const data = await response.json();
-      console.log(data);
-
-      let filmArray = [];
-      for (const key in data) {
-        filmArray.push({
-          key: key,
-          title: data[key].title,
-          crawl: data[key].openingText,
+  const filmsHandler = useCallback(async () => {
+    if (postRequest === false) {
+      setLoadingState(true);
+      try {
+        const response = await fetch(process.env.REACT_APP_DBLINKMOVIES, {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
         });
+        const data = await response.json();
+        console.log(data);
+
+        let filmArray = [];
+        for (const key in data) {
+          filmArray.push({
+            key: key,
+            title: data[key].title,
+            crawl: data[key].openingText,
+          });
+        }
+        console.log(filmArray);
+        setFilmDataState(filmArray);
+        setLoadingState(false);
+      } catch (error) {
+        setErrorState(error.message);
       }
-      console.log(filmArray);
-      setFilmDataState(filmArray);
-      setLoadingState(false);
-    } catch (error) {
-      setErrorState(error.message);
     }
-  }, []);
-  return [filmDataState, loadingState, errorState, getFilmsHandler];
+    else if (postRequest === true){
+        
+    }
+  }, [postRequest]);
+  return [filmDataState, loadingState, errorState, filmsHandler];
 }
 export default useHttp;
